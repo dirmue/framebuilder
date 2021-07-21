d4056003839bfefdfbe82cfadd65d860f4dfda45
Dirk Müller <dirmu@nihilverum.de>
2021-07-21 20:57:37 +0200

    call receive() without argument

--- tcp_test_rcv.py
+++ tcp_test_rcv.py
@@ -6,6 +6,6 @@ from framebuilder import tcp, tools
 h = tcp.TCPHandler.listen('eth0', int(sys.argv[1]),debug=False)
 start = time.time()
 while h.state != h.CLOSED:
-    print((h.receive(10000)).decode('utf-8'), end='')
+    print((h.receive()).decode('utf-8'), end='')
 print('\n---------------\ntransfer took', time.time() - start, 'seconds')
 print('\n\nBye!')
